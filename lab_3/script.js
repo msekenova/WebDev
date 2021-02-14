258c8586ff67f8e8662f757270ccaf263ba1bfd2
Meiirim <68385520+msekenova@users.noreply.github.com>
2021-02-14 19:13:18 +0600

    laboratory work #3

---
+++ lab_3/script.js
@@ -0,0 +1,56 @@
+
+	var list = document.getElementsByTagName("li");
+	for (var i = 0; i < list.length; i++) {
+  		var span = document.createElement("span");
+  		var txt = document.createTextNode("\uD83D\uDDF6");
+  		span.className = "close";
+  		span.appendChild(txt);
+  		list[i].appendChild(span);
+	}
+
+	var close = document.getElementsByClassName("close");
+	for (var i = 0; i < close.length; i++) {
+  		close[i].onclick = function() {
+    		var elem = this.parentElement;
+    		elem.remove();
+  		}
+	}
+
+
+
+
+document.querySelector("button").onclick = newTask;
+
+function newTask() {
+  var li = document.createElement("li");
+  var checkbox = document.createElement("input");
+  var del = document.createElement("del");
+  checkbox.type = "checkbox";
+  checkbox.className = "chk";
+  var inputValue = document.getElementById("new-task").value;
+  var t = document.createTextNode(inputValue);
+  del.appendChild(t);
+  li.appendChild(checkbox);
+  li.appendChild(del);
+  if (document.getElementById("new-task").value != "") {
+  	document.getElementById("un-list").appendChild(li);
+  }else {
+  	alert("Please, write at least 1 word");
+  }
+  
+  document.getElementById("new-task").value = null;
+
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\uD83D\uDDF6");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var elem = this.parentElement;
+      elem.remove();
+    }
+  }
+}
